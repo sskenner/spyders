d5b44522a0d191dfb5f78101523368d17cce16bb
aria <nimda@arias-Air.home>
2017-08-01 10:30:51 -0400

    initial commit

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+	name = "quotes"
+
+	start_urls = [
+		'http://quotes.toscrape.com/page/1/',
+		'http://quotes.toscrape.com/page/2/'
+	]
+
+	# def start_requests(self):
+	# 	urls = [
+	# 		'http://quotes.toscrape.com/page/1/',
+	# 		'http://quotes.toscrape.com/page/2/'
+	# 	]
+	# 	for url in urls:
+	# 		yield scrapy.Request(url=url, callback=self.parse)
+
+	def parse(self, response):
+		for quote in response.css('div.quote'):
+			
+
+
