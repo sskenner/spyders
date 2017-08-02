25a256957dab55fe53761be25990d997a4ef240c
aria <nimda@arias-Air.home>
2017-08-01 22:44:54 -0400

    update to include span selector for author extraction

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -11,9 +11,10 @@ class QuotesSpider(scrapy.Spider):
     	for quote in response.css('div.quote'):
     		yield {
     			'text': quote.css('span.text::text').extract_first(),
-    			'author': quote.css('small.author::text').extract_first(),
+    			'author': quote.css('span small::text').extract_first(),
     			'tags': quote.css('div.tags a.tag::text').extract()
     		}
+    		
     	next_page = response.css('li.next a::attr(href)').extract_first()
     	if next_page is not None:
     		yield response.follow(next_page, callback=self.parse)
