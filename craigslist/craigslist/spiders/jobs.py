cb0378894a68ee3db5f9c52a103913cfab26f79b
aria <nimda@arias-Air.home>
2017-08-02 13:50:06 -0400

    update jobs scrapy spider for craigslist

--- craigslist/craigslist/spiders/jobs.py
+++ craigslist/craigslist/spiders/jobs.py
@@ -4,8 +4,9 @@ import scrapy
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
-    allowed_domains = ['https://newyork.craigslist.org/search/egr']
-    start_urls = ['http://https://newyork.craigslist.org/search/egr/']
+    allowed_domains = ["craigslist.org"]
+    # allowed_domains = ['https://newyork.craigslist.org/search/egr']
+    start_urls = ['https://newyork.craigslist.org/search/egr']
 
     def parse(self, response):
         pass
