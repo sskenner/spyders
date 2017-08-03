260dfd27a760c6fd25535d36879d03a6f6022c48
sk <sskenner@gmail.com>
2017-08-02 20:21:38 -0400

    add function to extract job descriptions

--- craigslist/craigslist/spiders/jobs.py
+++ craigslist/craigslist/spiders/jobs.py
@@ -3,7 +3,7 @@ import scrapy
 from scrapy import Request
 
 class JobsSpider(scrapy.Spider):
-    name = 'jobscontent'
+    name = 'jobs'
     allowed_domains = ["craigslist.org"]
     start_urls = ['https://newyork.craigslist.org/search/egr']
 
@@ -19,6 +19,19 @@ class JobsSpider(scrapy.Spider):
     		yield Request(absolute_url, callback=self.parse_page, meta={'URL':absolute_url, 'Title':title, 'Address':address})
 
     	relative_next_url = response.xpath('//a[@class="button next"]/@href').extract_first()
-    	absolute_next_url = response.urljoin(relative_next_url)
+    	absolute_next_url = "https://newyork.craigslist.org" + relative_next_url
+        
+    	yield Request(absolute_next_url, callback=self.parse)
 
-    	yield Request(absolute_next_url)
+    def parse_page(self, response):
+        url = response.meta.get('URL')
+        title = response.meta.get('Title')
+        address = response.meta.get('Address')
+ 
+        description = "".join(line for line in response.xpath('//*[@id="postingbody"]/text()').extract()).strip()
+        
+        compensation = response.xpath('//p[@class="attrgroup"]/span[1]/b/text()').extract_first()
+        employment_type = response.xpath('//p[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+        yield{'URL':url, 'Title':title, 'Address':address, 'Description':description, 'Compensation':compensation, 'Employment_type':employment_type}
+
