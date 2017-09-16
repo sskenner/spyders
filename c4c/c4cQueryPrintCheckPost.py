19e9576f96ad628463658098a96615426e77250c
sk <sskenner@gmail.com>
2017-09-15 20:44:31 -0400

    update for clarity

--- c4c/c4cQueryPrintCheckPost.py
+++ c4c/c4cQueryPrintCheckPost.py
@@ -1,3 +1,8 @@
+# Python program to query custom search engine for listings and send them to an API
+
+# Name - c4cQueryPrintCheckPost.py
+# Date and version No: ??????
+
 import json, codecs, requests, pickle, datetime
 import urllib.request, urllib.parse, urllib.error
 import re
@@ -30,7 +35,7 @@ CODEFORCASH_API_KEY = '5b26197b391c5dab05c5606d43fba9c6'
 
 MAXIMUM_NUMBER_OF_SEARCH_RESULTS_PER_GOOGLE_API_QUERY = 10
 
-bad_words_list = ["personal trainer", "executive assistant", "low cost","ultimate software", "app tester", "1-2 hours a day","secretary", "front desk", "office manager", "use referr", "commission", "motivated individuals" , "sales specialist","no experience required", "amazing opportunity", "court researcher","technical support", "tech support", "mystery shopper","customer service", "field engineer", "administrative", "book keeping", "extra money", "extra cash", "extra income", "data entry", "have a car", "debit card", "earn extra income", "step by step training", "dollars a week", "supplemental income", "sales rep","closed lead", "do you want to make", "facebook page", "facebook fan page", "theater installation", "game tester", "% stake","printing and mailing", "laserjet", "credit score","real estate investment", "research study","in person", "focus group", "survey", "you must live", "local", "must be local", "tutor", "instructor", "partner ", "equity", "cofounder", "co founder", "co-founder", "unpaid", "volunteer", "get paid", "get pay", "weekly", "webcam", "money making", "fast money", "workfromhome", "fast cash", "scam", "make money", "selling cell phones", "wireless sales", "it's legit", "telemarketer", "fb account", "Cell Phone Repair", "earn money by just", "only applicants residing in", "from his location", "virtual assistant", "by working less", "earn extra money", "experienced seller", "looking for a job", "earn over", "motorclub",  "office assistant", "event planner", "____________________________________________________________", "Filter by:"]
+BAD_WORDS_LIST = ["personal trainer", "executive assistant", "low cost","ultimate software", "app tester", "1-2 hours a day","secretary", "front desk", "office manager", "use referr", "commission", "motivated individuals" , "sales specialist","no experience required", "amazing opportunity", "court researcher","technical support", "tech support", "mystery shopper","customer service", "field engineer", "administrative", "book keeping", "extra money", "extra cash", "extra income", "data entry", "have a car", "debit card", "earn extra income", "step by step training", "dollars a week", "supplemental income", "sales rep","closed lead", "do you want to make", "facebook page", "facebook fan page", "theater installation", "game tester", "% stake","printing and mailing", "laserjet", "credit score","real estate investment", "research study","in person", "focus group", "survey", "you must live", "local", "must be local", "tutor", "instructor", "partner ", "equity", "cofounder", "co founder", "co-founder", "unpaid", "volunteer", "get paid", "get pay", "weekly", "webcam", "money making", "fast money", "workfromhome", "fast cash", "scam", "make money", "selling cell phones", "wireless sales", "it's legit", "telemarketer", "fb account", "Cell Phone Repair", "earn money by just", "only applicants residing in", "from his location", "virtual assistant", "by working less", "earn extra money", "experienced seller", "looking for a job", "earn over", "motorclub",  "office assistant", "event planner", "____________________________________________________________", "Filter by:"]
 
 def do_google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
@@ -43,23 +48,23 @@ def do_google_search(search_term, api_key, cse_id, **kwargs):
                     {
                         'title': 'NO MORE RECORDS',
                         'link': 'https://jobs.lever.co/xyz'
-                        # in case lever 404 stops working
+                        # use in case lever 404 stops working
                         # 'link': 'https://github.com/sskenner/xyz'
                     }
                 ]
             }
-        # visual of when reach the gse page when totalResults == '0'
-        print('MANUAL ASSIGNMENT OF RES')
+        # # prints when reach the gse page where totalResults == '0'
+        # print('MANUAL ASSIGNMENT OF RES')
         return res['items']
     else:
         return res['items']
     
-
+# set gse query parameters to iterate through results
 def get_job_listings_from_google(number_of_listings_to_get):
     return_value = []
     for search_result_number_from_which_api_query_results_start in range(1, number_of_listings_to_get + 1, MAXIMUM_NUMBER_OF_SEARCH_RESULTS_PER_GOOGLE_API_QUERY):
         return_value.extend(do_google_search(
-            # https://i.codefor.cash/job_alerts/generate_subscriber_keywords
+            # online version of keywords > https://i.codefor.cash/job_alerts/generate_subscriber_keywords
             search_term="haskell 'software engineer'",
             api_key=API_KEY_TO_USE_FOR_THIS_RUN, cse_id=CSE_ID_TO_USE_FOR_THIS_RUN,
             num=MAXIMUM_NUMBER_OF_SEARCH_RESULTS_PER_GOOGLE_API_QUERY, start=search_result_number_from_which_api_query_results_start))
@@ -71,13 +76,13 @@ def save_gse_call_results(listings):
                 indent = 4))
 
 def send_job_listings_to_codeforcash(listings):
+    # iterate through gse results to grab values for c4c API
     for listing in range(len(listings)):
         any_bad_words = False
         data_to_send_in_request_body = {
             "key": CODEFORCASH_API_KEY,
             "title": listings[listing]["title"],
             "website": listings[listing]["link"],
-            # "description": listings[listing]["snippet"],
             "description": "",
             "utc_datetime": datetime.datetime.utcnow().isoformat(),
             "lat": "",
@@ -87,32 +92,35 @@ def send_job_listings_to_codeforcash(listings):
             "remote_ok": "",
             "time_commitment": ""
         }
+        # encode/decode dictionary from and to json
         data_send_job_lisitings_to_codeforcash = json.dumps(data_to_send_in_request_body)
         data_of_each_listing = json.loads(data_send_job_lisitings_to_codeforcash)
 
+        # use beautiful soup to follow url and grab descriptions
         try:
             html = urllib.request.urlopen(data_of_each_listing["website"]).read()
+        # keep going if url is 404
         except urllib.error.HTTPError as e:
             print(e)
             continue
         else:
             only_tag_class = SoupStrainer("div", {"class" : "section-wrapper page-full-width"})
             soup = BeautifulSoup(html, "html.parser", parse_only=only_tag_class)
-            htmlDecode = soup.encode('utf-8').decode('utf-8', 'ignore')
+            html_decoded = soup.encode('utf-8').decode('utf-8', 'ignore')
             
             f = open('Lynx.htm','w')
-            f.write(htmlDecode)
+            f.write(html_decoded)
             f.close()
 
-            #refactor as functions
+            # pass description through lynx to format
             web_data = ''
             cmd = 'lynx -dump -width 1024 -nolist -notitle \"{0}\"'.format('./Lynx.htm')
             lynx = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
             web_data = lynx.stdout.read()
             web_data = web_data.decode('utf-8', 'replace')
 
-            #move to outer loop above.. break or continue?
-            for bad_word in bad_words_list:
+            # check for bad words in description
+            for bad_word in BAD_WORDS_LIST:
                 if bad_word in web_data:
                     any_bad_words = True
                     print('bad word found')
@@ -122,13 +130,13 @@ def send_job_listings_to_codeforcash(listings):
                     for data_key in data_to_send_in_request_body:
                         data_to_send_in_request_body[data_key] = data_to_send_in_request_body[data_key]
                     
-            
+            # save listings without bad words
             if any_bad_words == False:
                 clean_data_to_post = data_to_send_in_request_body
             else:
                 continue
                     
-            # test print json formatted complete listing
+            # # test print json formatted complete listing
             # print(data_to_send_in_request_body)
         print(clean_data_to_post)   
 
