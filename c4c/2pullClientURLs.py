213717bcd1e81ccb9052eaec35b4cbf0646d640d
santana kenner <sskenner@gmail.com>
2017-10-09 17:57:49 +0000

    update client list to include non posted boards

--- c4c/2pullClientURLs.py
+++ c4c/2pullClientURLs.py
@@ -1,15 +1,36 @@
-import re
+# import re
+import json
 
-han = open('leverClientListURIList.txt')
+handle = open('leverClientAggrList.txt')
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+        # print(counts)
+        # with open('leverClientListCount.txt','a+') as f:
+        #     f.write(json.dumps(counts))
+print(sorted([(v,k) for k,v in counts.items()]))
+
+# bigcount = None
+# bigword = None
+# for word, count in counts.items():
+#     if bigcount is None or count > bigcount:
+#         bigword = word
+#         bigcount = count
+# print(bigword, bigcount)
+
+# han = open('leverClientListURIList.txt')
 # han = open('leverClientListGQueryByDate.1.txt')
 # han = open('leverClientListURIListTest.txt')
 # han = open('leverClientListGQueryByDate.txt')
 
-for line in han:
-    line = line.rstrip()
-    client = re.findall('(^.+)/', line)
-    with open('leverClientList.2.txt','a+') as f:
-            f.write(line)
+# for line in han:
+#     line = line.rstrip()
+#     client = re.findall('(^.+)/', line)
+#     with open('leverClientList.2.txt','a+') as f:
+#             f.write(line)
     # print(client)
 
     # if re.search('https://jobs', line) :
