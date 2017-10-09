e025cd11284327895c33d5bfec5b6ad8c0121df8
santana kenner <sskenner@gmail.com>
2017-10-09 14:10:25 +0000

    query G by date by -client to update client list

--- c4c/2pullClientURLs.py
+++ c4c/2pullClientURLs.py
@@ -1,12 +1,18 @@
 import re
 
-han = open('leverClientListURIListTest.txt')
+han = open('leverClientListURIList.txt')
+# han = open('leverClientListGQueryByDate.1.txt')
+# han = open('leverClientListURIListTest.txt')
 # han = open('leverClientListGQueryByDate.txt')
 
 for line in han:
     line = line.rstrip()
     client = re.findall('(^.+)/', line)
-    print(client)
+    with open('leverClientList.2.txt','a+') as f:
+            f.write(line)
+    # print(client)
 
     # if re.search('https://jobs', line) :
-    #     print(line)
+    #     with open('leverClientListURIList.txt','a+') as f:
+    #         f.write(line)
+        # print(line)
